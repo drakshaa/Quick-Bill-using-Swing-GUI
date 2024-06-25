1d39cfbe3379f8bc70608c1e540f838ce7f9a354
Draksha Chaudhary <draksha_ug_23@cse.nits.ac.in>
2024-06-25 10:16:41 +0545

    Quick bill

---
+++ src/com/model/CashierDetail.java
@@ -0,0 +1,55 @@
+package com.model;
+
+public class CashierDetail {
+
+	private int id;
+	private String name;
+	private int phone_no;
+	private String email;
+	private String address;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone_no() {
+		return phone_no;
+	}
+	public void setPhone_no(int phone_no) {
+		this.phone_no = phone_no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "CashierDetail [id=" + id + ", name=" + name + ", phone_no=" + phone_no + ", email=" + email
+				+ ", address=" + address + ", password=" + password + "]";
+	}
+	
+}
